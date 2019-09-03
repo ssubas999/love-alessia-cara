ed86c777bc20ece3f9713955c393e82a1444d632
Subas Subedi <ssubas999@gmail.com>
2019-09-03 16:59:01 +0000

    Minor change in code and added comments in order to make it easier for future contributors

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 # app.py
+
+#importing necessary libraries
 import flask, random, os, tweepy, requests, json, requests_oauthlib
 
 app = flask.Flask(__name__)
 
-@app.route('/')  
+@app.route('/')
 def index():
     
-    # Twitter API using requests_oauthlib
+    # Twitter API using requests_oauthlib(OAuth1)
+    # Twitter API url abstracted for screen_name = 'alessiacara' that will get 20 tweets in response
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=alessiacara&exclude_replies=true&count=20"
+    # Submitting Consumer API keys and Access token & access token secret
     oauth = requests_oauthlib.OAuth1(
         "M0V2et56cbWEztaGyng2sRs0v",
         "95tvu7fmfyHMidMLjfVQjsMizuAnLnG00iHoERj88PIfoYq8Fx",
@@ -19,13 +23,12 @@ def index():
     # Using pyton's json library to make it more readable (in commented line below)
     # print(json.dumps(json_body1, indent = 2))
     
-    
+    # Digging through the JSON object in order to abstract tweets(tweets_list) and followers count(fc)
     tweets_list = []
     for content in json_body1:
         tweet = content["text"]
         tweets_list.append(tweet)
     fc = json_body1[0]["user"]["followers_count"]
-    print(fc)
     random_num0 = random.randint(0,(len(tweets_list)-1))
     rt = tweets_list[random_num0]
     
@@ -47,30 +50,35 @@ def index():
     rt = tweet_list[random_num1]
     '''
     
-    #Genius api
+    # Genius API using Bearer autorization
+    # Genious API url abstracted by artist id (394321), sorted by popularity and will get 20 responses per load
     url = "https://api.genius.com/artists/394321/songs?sort=popularity&page=1&per_page=20"
     my_headers = {"Authorization": "Bearer Bxi5CY7701gxpWiHC32WaItkXKU2Auk0v2p5K30rVEvOFfvq9xMg61Ig1mR_xnSQ"}
     response = requests.get(url, headers = my_headers)
-    # .json() will return the response as json formatted string (more like a combination of lists and dictionary)
-    json_body2 = response.json()
+    # Using pyton's json library to make it more readable | .json() will return the response as json formatted string (more like a combination of lists and dictionary)
     # The u- prefix just means(when you print json data) that you have a Unicode string. When you really use the string, it won't appear in your data.
-    # Using pyton's json library to make it more readable
     # print(json.dumps(json_body2, indent = 2))
+    json_body2 = response.json()
     
-    # Creating list of image urls.
+    # Digging through JSON object
     img_url_list = []
     album_title_list =  []
     lyrics_url_list = []
     for inside_songs in json_body2['response']['songs']:
+        
+        # Abstracting list of song_art_image_url
         img_url = inside_songs['song_art_image_url']
         img_url_list.append(img_url)
         
+        # Abstracting list of title_with_featured
         album_title = inside_songs['title_with_featured']
         album_title_list.append(album_title)
         
+        # Abstracting list of url
         lyrics_url = inside_songs['url']
         lyrics_url_list.append(lyrics_url)
-
+        
+    # Choosing a random song out of 20 songs.
     random_num2 = random.randint(0,(len(album_title_list)-1))
     ra = album_title_list[random_num2]
     riu = img_url_list[random_num2]
